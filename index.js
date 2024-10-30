4547d76dd14762b6783cd89a5096c218199f69bb
Ethan Collins (TMNA) <ethan.collins1@toyota.com>
2024-10-30 13:30:38 -0500

    setup caprover deployment

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send({ message: "Hello World!" }));
+app.get("/", (_, res) => res.send({ message: "Hello World!" }));
 
-app.all("*", (req, res) => res.status(404).send({ message: "Route not found" }));
+app.all("*", (_, res) => res.status(404).send({ message: "Route not found" }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
